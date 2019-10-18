02f8eee0992c3884bd5eb4fc79ab40b0bb7065be
Raulzito311 <raulmff@gmail.com>
2019-10-18 16:08:13 -0300

    Excluindo lancamentos e o decorador @ViewChild()

--- src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { LazyLoadEvent } from 'primeng/components/common/api';
 
 import { LancamentoService, LancamentoFiltro } from './../lancamento.service';
+import { Table } from 'primeng/table';
 
 @Component({
   selector: 'app-lancamentos-pesquisa',
@@ -16,6 +17,8 @@ export class LancamentosPesquisaComponent implements OnInit {
   totalRegistros = 0;
 
   lancamentos = [];
+
+  @ViewChild('tabela', {static: true}) grid: Table;
 
   cols = [
     { field: 'pessoa', header: 'Pessoa' },
@@ -42,6 +45,13 @@ export class LancamentosPesquisaComponent implements OnInit {
   aoMudarPagina(event: LazyLoadEvent) {
     const pagina = event.first / event.rows;
     this.pesquisar(pagina);
+  }
+
+  excluir(lancamento: any) {
+    this.lancamentoService.excluir(lancamento.codigo)
+      .then(() => {
+        this.grid.reset();
+      });
   }
 
   getHeaderColumnClasses(header: string) {
