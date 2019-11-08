1a9abd05d6a6fdd03935b8227b7e1ee3b342a388
Raulzito311 <raulmff@gmail.com>
2019-11-08 18:33:37 -0300

    Tratando casos de erros e sucesso de autenticação

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -28,11 +28,16 @@ export class AuthService {
     return this.http.post(this.oauthTokenURL, body, { headers })
       .toPromise()
       .then(response => {
-        console.log(response);
         this.armazenarToken(response['access_token']);
       })
       .catch(response => {
-        console.log(response);
+        if (response.status === 400) {
+          if (response.error.error === 'invalid_grant') {
+            return Promise.reject('Usuário ou senha inválidos!');
+          }
+        }
+
+        return Promise.reject(response);
       });
   }
 
--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ErrorHandlerService } from './../../core/error-handler.service';
 
 import { AuthService } from './../auth.service';
 
@@ -9,10 +12,20 @@ import { AuthService } from './../auth.service';
 })
 export class LoginFormComponent {
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private errorHandler: ErrorHandlerService,
+    private router: Router
+    ) { }
 
   login(usuario: string, senha: string) {
-    this.auth.login(usuario, senha);
+    this.auth.login(usuario, senha)
+      .then(() => {
+        this.router.navigate(['/lancamentos']);
+      })
+      .catch(erro => {
+        this.errorHandler.handle(erro);
+      });
   }
 
 }
